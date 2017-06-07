1f2bccac7014dc50e612b4a32e8e9933bb7f0530
Eve <yuanyichen1@gmail.com>
2017-06-07 07:46:01 -0700

    rs

--- HW4_Win - See data_1.R
+++ HW4_Win - See data_1.R
@@ -11,7 +11,18 @@ setwd("C:/Users/Eve/Dropbox/UCLA Files/Courses/418 Tools of Data Science/STATS 4
 #Load packages
 library(readr)
 library(ROCR)
-
+library(ggplot2)
+library(dplyr)
 #CSV
-dat <- read.csv("test.csv")
-summary(dat)
+data <- read.csv("test.csv")
+summary(data)
+
+#Job vs y
+data_job = data %>%
+  group_by(job) %>%
+  summarise(n = n())
+
+ggplot(data=data_job, aes(x=job, y=n))+
+  geom_bar(stat = "identity", aes(fill = job))+ 
+  theme_bw()+ coord_flip() + 
+  xlab("Different Types of Jobs") + ylab("Numbers")
